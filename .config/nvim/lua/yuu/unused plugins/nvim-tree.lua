98cb13dda62fb5f5cec07e2169d0c77c425b5b20
Yuu Murasaki <trinhducanh69@gmail.com>
2024-10-01 21:30:15 +0700

    add

---
+++ .config/nvim/lua/yuu/unused plugins/nvim-tree.lua
@@ -0,0 +1,31 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    event = "VeryLazy",
+    dependencies = "nvim-tree/nvim-web-devicons",
+
+    config = function()
+        local nvimtree = require("nvim-tree")
+
+        nvimtree.setup({
+            view = {
+                width = 30,
+            },
+            
+            filters = {
+                custom = { ".DS_Store" },
+            },
+
+            actions = {
+                open_file = {
+                    window_picker = {
+                        enable = false,
+                    },
+                },
+            },
+
+            git = {
+                ignore = false,
+            },
+        })
+    end
+}
