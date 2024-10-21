f0a8b4accd18ef2aa25d8a0a2bf5fefcdee99e43
YuuMurasaki <hoangthibaongoc411@gmail.com>
2024-10-21 23:03:52 +0700

    add

--- .config/nvim/lua/yuu/unused plugins/which-key.lua
+++ .config/nvim/lua/yuu/unused plugins/which-key.lua
@@ -1,9 +1,9 @@
 return {
     "folke/which-key.nvim",
     event = "VeryLazy",
-    
+
     init = function()
         vim.o.timeout = true
-        vim.o.timeoutlen = 1000
+        vim.o.timeoutlen = 300
     end,
 }
