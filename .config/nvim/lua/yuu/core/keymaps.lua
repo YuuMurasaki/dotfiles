da1a5f515675d925c9650728e1d85a10f9cff2e1
YuuMurasaki <hoangthibaongoc411@gmail.com>
2024-10-26 21:41:39 +0700

    hehe

--- .config/nvim/lua/yuu/core/keymaps.lua
+++ .config/nvim/lua/yuu/core/keymaps.lua
@@ -1,22 +1,25 @@
 -- Clear search highlights
-vim.keymap.set("n", "<Esc>", "<cmd>noh<CR>", { desc = "general clear highlights" })
+vim.keymap.set("n", "<Esc>", "<cmd>noh<CR>", { desc = "general clear highlights", silent = true })
 
 -- Select all
-vim.keymap.set("n", "<leader>a", "ggVG", { desc = "Select whole buffer" })
+vim.keymap.set("n", "<leader>a", "ggVG", { desc = "Select whole buffer", silent = true })
 
 -- Split windows movements
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to the left split" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to the split below" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to the split above" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to the right split" })
+vim.keymap.set("n", "<C-h>", "<cmd>wincmd h<CR>", { desc = "Move to the left split", silent = true })
+vim.keymap.set("n", "<C-j>", "<cmd>wincmd j<CR>", { desc = "Move to the split below", silent = true })
+vim.keymap.set("n", "<C-k>", "<cmd>wincmd k<CR>", { desc = "Move to the split above", silent = true })
+vim.keymap.set("n", "<C-l>", "<cmd>wincmd l<CR>", { desc = "Move to the right split", silent = true })
 
 -- Move highlighted text
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
 
--- Better half page down/up
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- Better cursor placement
+vim.keymap.set("n", "J", "mzJ`z", { silent = true })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { silent = true })
+vim.keymap.set("n", "n", "nzzzv", { silent = true })
+vim.keymap.set("n", "N", "Nzzzv", { silent = true })
 
 -- Replace the word you're on
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
