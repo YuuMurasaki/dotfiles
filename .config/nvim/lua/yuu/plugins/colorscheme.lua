a4aa102eb12bfbc110d5560a1178bbc58291e381
YuuMurasaki <hoangthibaongoc411@gmail.com>
2024-10-31 21:11:06 +0700

    hehe

--- .config/nvim/lua/yuu/plugins/colorscheme.lua
+++ .config/nvim/lua/yuu/plugins/colorscheme.lua
@@ -1,34 +1,58 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            vim.cmd([[colorscheme catppuccin]])
-        end,
-    },
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
 
-    {
-        "folke/tokyonight.nvim",
-        name = "tokyo-night",
-        priority = 1000,
-    },
+    config = function()
+        require("catppuccin").setup({
+            flavour = "auto", -- latte, frappe, macchiato, mocha
+            background = { -- :h background
+                light = "latte",
+                dark = "mocha",
+            },
 
-    {
-        "tanvirtin/monokai.nvim",
-        name = "monokai",
-        priority = 1000,
-    },
-    
-    {
-        "Mofiqul/dracula.nvim",
-        name = "dracula",
-        priority = 1000,
-    },
+            transparent_background = false, -- disables setting the background color.
+            show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+            term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
 
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        priority = 1000,
-    },
+            dim_inactive = {
+                enabled = false, -- dims the background color of inactive window
+                shade = "dark",
+                percentage = 0.15, -- percentage of the shade to apply to the inactive window
+            },
+
+            no_italic = false, -- Force no italic
+            no_bold = false, -- Force no bold
+            no_underline = false, -- Force no underline
+
+            styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+                comments = { "italic" }, -- Change the style of comments
+                conditionals = { "italic" },
+                loops = {},
+                functions = {},
+                keywords = {},
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = {},
+                operators = {},
+                -- miscs = {}, -- Uncomment to turn off hard-coded styles
+            },
+
+            color_overrides = {},
+            custom_highlights = {},
+            default_integrations = true,
+
+            integrations = {
+                cmp = true,
+                treesitter = true,
+                -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+            },
+        })
+
+        -- setup must be called before loading
+        vim.cmd.colorscheme "catppuccin"
+    end,
 }
