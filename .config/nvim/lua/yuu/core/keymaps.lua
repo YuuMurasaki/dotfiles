1a0694d87769dd4a91253450169f258a4ec47e4a
YuuMurasaki <hoangthibaongoc411@gmail.com>
2024-11-01 12:41:54 +0700

    change keymap

--- .config/nvim/lua/yuu/core/keymaps.lua
+++ .config/nvim/lua/yuu/core/keymaps.lua
@@ -14,12 +14,15 @@ vim.keymap.set("n", "<C-l>", "<cmd>wincmd l<CR>", { desc = "Move to the right sp
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
 
--- Better cursor placement
+-- Keep window centered when going up/down
 vim.keymap.set("n", "J", "mzJ`z", { silent = true })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { silent = true })
 vim.keymap.set("n", "n", "nzzzv", { silent = true })
 vim.keymap.set("n", "N", "Nzzzv", { silent = true })
+
+-- Get out Q
+vim.keymap.set("n", "Q", "<nop>")
 
 -- Replace the word you're on
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
--- .config/nvim/lua/yuu/plugins/telescope.lua
+++ .config/nvim/lua/yuu/plugins/telescope.lua
@@ -32,8 +32,8 @@ return {
         vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>', { desc = "Find files", silent = true })
         vim.keymap.set('n', '<leader>fr', '<cmd>Telescope oldfiles<CR>', { desc = "Find recent files", silent = true })
         vim.keymap.set('n', '<leader>fs', '<cmd>Telescope live_grep<CR>', { desc = "Find string", silent = true })
+        vim.keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd", silent = true })
         vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { desc = "Find buffers", silent = true })
         vim.keymap.set('n', '<leader>fm', '<cmd>Telescope marks<CR>', { desc = "Find marks", silent = true })
-        vim.keymap.set('n', '<leader>fc', '<cmd>Telescope colorscheme<CR>', { desc = "Choose colorscheme", silent = true })
     end,
 }
