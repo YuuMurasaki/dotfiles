458432e4f1f13129ed288d597cb078516388b594
YuuMurasaki <hoangthibaongoc411@gmail.com>
2024-11-02 22:49:56 +0700

    nope

--- .config/nvim/lua/yuu/unused_plugins/formatting.lua
+++ .config/nvim/lua/yuu/unused_plugins/formatting.lua
@@ -9,19 +9,12 @@ return {
                 lua = { "stylua" },
                 cpp = { "clang-format" },
             },
+
             format_on_save = {
                 lsp_fallback = true,
                 async = false,
                 timeout_ms = 1000,
             },
         })
-
-        vim.keymap.set({ "n", "v" }, "<leader>=", function()
-            conform.format({
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 1000,
-            })
-        end, { desc = "Format file or range (in visual mode)" })
     end,
 }
