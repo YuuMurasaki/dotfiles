f335a93725e0fd6d3a443274473e707bd2e32ce7
YuuMurasaki <hoangthibaongoc411@gmail.com>
2024-11-03 17:22:27 +0700

    remove pulgins

--- .config/nvim/lua/yuu/plugins/completion-snippets.lua
+++ .config/nvim/lua/yuu/plugins/completion-snippets.lua
@@ -75,7 +75,7 @@ return {
 
             -- sources for autocompletion
             sources = cmp.config.sources({
-                { name = "nvim_lsp"},
+                { name = "nvim_lsp"}, -- LSP suggestions
                 { name = "luasnip" }, -- snippets
                 { name = "buffer" }, -- text within current buffer
                 { name = "path" }, -- file system paths
