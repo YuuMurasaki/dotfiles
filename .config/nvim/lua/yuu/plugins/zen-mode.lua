cb7f7c5412a3ad7a88d08ef09c7e8d378b996f7c
YuuMurasaki <hoangthibaongoc411@gmail.com>
2024-11-09 22:29:16 +0700

    some minor changes

---
+++ .config/nvim/lua/yuu/plugins/zen-mode.lua
@@ -0,0 +1,14 @@
+return {
+    "folke/zen-mode.nvim",
+    dependencies = {
+        "folke/twilight.nvim",
+    },
+
+    keys = {
+        { "<leader>z", "<cmd>ZenMode<CR>", { noremap = true, silent = true } },
+    },
+
+    config = function()
+        require("zen-mode").setup()
+    end
+}
---
+++ Workspace/testing/1code.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int32_t i = 0; i <= 5; i++)
+        cout << "Hello World\n"; // this is hello world
+
+    return 0;
+}
