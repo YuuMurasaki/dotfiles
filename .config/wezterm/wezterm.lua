664596b85afea53f88940a5c4dd478dd62b9530b
YuuMurasaki <hoangthibaongoc411@gmail.com>
2024-11-14 22:01:52 +0700

    nothing

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,17 +4,16 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.default_prog = { "/bin/bash" }
+config.default_prog = { '/bin/bash' }
 
 config.color_scheme = "Catppuccin Mocha" -- or Macchiato, Frappe, Latte
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
+
 config.font_size = 14
+config.use_fancy_tab_bar = false
 
-config.enable_tab_bar = true
-config.hide_tab_bar_if_only_one_tab = true
-
-config.window_decorations = "RESIZE"
+config.window_decorations = "NONE"
 
 config.window_padding = {
     left = 0,
